e6b229feac8879e8e2a3d21995a7829e94d0bce0
Muhammad007 <mohammadbink2007@gmail.com>
2025-05-21 11:23:25 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var fnumber = Number(prompt("Enter your First Number to add"))
+var snumber = Number(prompt("Enter your Second Number to add"))
+alert(fnumber+snumber)
+
+var fnumber = Number(prompt("Enter your First Number to subtract"))
+var snumber = Number(prompt("Enter your Second Number to subtract"))
+alert(fnumber-snumber)
+
+var fnumber = Number(prompt("Enter your First Number to multiply"))
+var snumber = Number(prompt("Enter your Second Numberto multiply"))
+alert(fnumber*snumber)
+
+var fnumber = Number(prompt("Enter your First Number to divide"))
+var snumber = Number(prompt("Enter your Second Numberto divide"))
+alert(fnumber/snumber)
+
